dca382af57f1cb21cff2dedf7afd42360c277b63
Mark Tyler <mark.tyler@tdsinc.com>
2015-03-09 19:45:04 -0500

    Save and load tasks to/from local storage.

--- js/listly.js
+++ js/listly.js
@@ -1,22 +1,52 @@
 var Listly = function() {
 
   function Listly() {
-    this.tasks = [];
     var self = this;
+    self.tasks = [];
 
+    function addTask(name) {
+      name = $(name);
+      self.tasks.push(name.val());
+      if (save()) {
+        var result = $('#tasks').append('<li>' + name.val() +'</li>');
+        result.find('li').addClass('list-group-item');
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
+
+    function load() {
+      self.tasks = JSON.parse(localStorage.tasks);
+      $.each(self.tasks, function(index, task) {
+        $('#tasks').append('<li class="list-group-item">' + task +'</li>');
+      });
+    }
+
+    function save() {
+      try {
+        return (localStorage.tasks = JSON.stringify(self.tasks));
+      }
+      catch(err) {
+        return false;
+      }
+    }
+
+    // load the form
+    load();
+
+    // instantiate handlers
     $('form#new_task').submit(function(ev) {
       ev.preventDefault();
       var task_name = $(this.task_name);
 
-      // Add list item to #tasks
-      var result = $('#tasks').append('<li>' + task_name.val() +'</li>');
-      result.find('li').addClass('list-group-item');
-      task_name.val('');
-      task_name.focus();
-
-      // Add the task name to tasks array
-
+      if (addTask(task_name)) {
+        task_name.val('');
+      }
+      task_name.focus().select();
     });
+
   }
 
   return Listly;
